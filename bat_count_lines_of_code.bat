73ede8b22edefabe19f799f077f3a1c06307cc3c
Vignesh Gunasekaran <vigneshgunasekaran96@gmail.com>
2022-12-27 20:55:53 +0100

    Fixed cloc script Added descriptor buffer extension

--- bat_count_lines_of_code.bat
+++ bat_count_lines_of_code.bat
@@ -1,4 +1,18 @@
-SET "mypath=%~dp0azhal\src"
+@echo OFF
+SET "COMMON_DIR=%~dp0common\src"
+SET "AZHAL_DIR=%~dp0azhal\src"
+SET "SANDBOX_DIR=%~dp0sandbox\src"
 
-azhal\external\cloc\cloc.exe %mypath%
+echo Project: Common
+tools\cloc\cloc.exe %COMMON_DIR%
+echo:
+
+echo Project: Azhal
+tools\cloc\cloc.exe %AZHAL_DIR%
+echo:
+
+echo Project: Sandbox
+tools\cloc\cloc.exe %SANDBOX_DIR%
+echo:
+
 PAUSE
